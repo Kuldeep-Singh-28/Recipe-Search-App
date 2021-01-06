d23bbb6feb0038dca2b0be39430c573485686197
Kuldeep28-star <kuldeepsingh281999@gmail.com>
2021-01-07 01:00:14 +0530

    deploy-stage2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Recipe from './Recipe';
-require('dotenv').config({path: __dirname + '/.env'})
+import Recipe from "./Recipe";
+require("dotenv").config({ path: __dirname + "/.env" });
 
 function App() {
   const APP_ID = process.env.REACT_APP_APP_ID;
@@ -9,42 +9,58 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('cake');
+  const [query, setQuery] = useState("cake");
 
   useEffect(() => {
     getRecipes();
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    // console.log(data.hits);
   };
 
-  const updateSearch = e => {
-    setSearch(e.target.value)
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
   };
 
-  const getSearch = e => {
+  const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
-    setSearch('');
+    setSearch("");
   };
 
   return (
     <div className="App">
-      <form action="" className="search-form" onSubmit={getSearch} >
-        <input type="text" className="search-bar" placeholder="Enter name of Dish" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">Search</button>
+      <form action="" className="search-form" onSubmit={getSearch}>
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Enter name of Dish"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
       <div className="recipes">
-        {recipes.map(recipe => (
-          <Recipe key={recipe.recipe.label} calories={Math.floor(recipe.recipe.calories)} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} title={recipe.recipe.label} />
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            calories={Math.floor(recipe.recipe.calories)}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            title={recipe.recipe.label}
+          />
         ))}
       </div>
     </div>
   );
-};
+}
 
-export default App
+export default App;
